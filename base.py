8bdefa76839266f601965f0f1ac68cc9fed15e88
Guy Markman <dht1nre@hotmail.com>
2016-12-05 11:37:28 +0200

    base

---
+++ base.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# By Alon Bar-Lev @alonbl
+"""Base module"""
+
+
+import logging
+
+
+class Base(object):
+    """Base of all objects"""
+
+    LOG_PREFIX = 'my'
+
+    @property
+    def logger(self):
+        """Logger."""
+        return self._logger
+
+    def __init__(self):
+        """Contructor."""
+        self._logger = logging.getLogger(
+            '%s.%s' % (
+                self.LOG_PREFIX,
+                self.__module__,
+            ),
+        )
+
+
+def setup_logging(stream=None, level=logging.INFO):
+    logger = logging.getLogger(Base.LOG_PREFIX)
+    logger.propagate = False
+    logger.setLevel(level)
+
+    try:
+        if stream is not None:
+            h = logging.StreamHandler(stream)
+            h.setLevel(logging.DEBUG)
+            h.setFormatter(
+                logging.Formatter(
+                    fmt=(
+                        '%(asctime)-15s '
+                        '[%(levelname)-7s] '
+                        '%(name)s::%(funcName)s:%(lineno)d '
+                        '%(message)s'
+                    ),
+                ),
+            )
+            logger.addHandler(h)
+    except IOError:
+        logging.warning('Cannot initialize logging', exc_info=True)
+
+    return logger
