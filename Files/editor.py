f881e548d972708cd11a0364a1045980ff06f189
Water1234562 <dht1nre@hotmail.com>
2017-04-05 10:33:26 +0300

    New version of editor, for the current build of the .xml files

--- Files/editor.py
+++ Files/editor.py
@@ -1,5 +1,4 @@
 import os
-import string
 from xml.etree import ElementTree
 
 from . import util
@@ -8,37 +7,40 @@ N_ANSWERS = 4
 
 
 def main():
-    quiz = ElementTree.Element("quiz")
-    settings = ElementTree.SubElement(quiz, "settings")
-    name = raw_input("Name of the quiz. ")
-    n_questions = raw_input("How many question in the quiz? ")
-    ElementTree.SubElement(settings, 'name').text = name
-    ElementTree.SubElement(settings, 'number_of_questions').text = n_questions
-    questions = ElementTree.SubElement(quiz, "questions")
-    for x in range(int(n_questions)):
-        q = ElementTree.SubElement(questions, "question", number=str(x + 1))
-        ElementTree.SubElement(q, "question_text").text = raw_input(
-            "What is the question? ")
-        ElementTree.SubElement(q, "picture").text = raw_input(
-            "Picture name. Must include extension. Leave Blunk for nothing.")
-        for z in range(N_ANSWERS):
-            answer = ElementTree.SubElement(
-                q, "answer", number=string.ascii_uppercase[z])
-            ElementTree.SubElement(
-                answer, "answer_text").text = raw_input("Enter the answer ")
-            ElementTree.SubElement(answer, "right_wrong").text = raw_input(
-                "Is the answer right or wrong? (answer in true or false)")
-    build = util.prettify(quiz)
+    Root = ElementTree.Element("Root")
+    ElementTree.SubElement(Root, "Quiz", {
+        "name": raw_input("Name of the quiz. "),
+        "number_of_questions": raw_input("How many question in the quiz? ")
+    })
+    for question in Root.find("./Quiz").attrib["number_of_questions"]:
+        q = ElementTree.SubElement(
+            Root.find("./Quiz"), {
+                "duration": raw_input("How long is this question? ")
+            })
+        ElementTree.SubElement(q, "Text").text = "<![CDATA[%s]]>" % (
+            raw_input("What is the question?"))
+        for answer in range(N_ANSWERS):
+            ans = ElementTree.SubElement(
+                q, "Answer").text = raw_input("Enter the answer ")
+            right_wrong = raw_input(
+                "Is the answer right or wrong? (answer in true or false) ")
+            if right_wrong == "true":
+                ans.attrib["correct"] = "1"
+    build = util.prettify(Root)
     print build
+    name = Root.find("./Quiz").attrib["name"]
     try:
         fd = os.open("../Quizes/%s.xml" % name, os.O_CREAT | os.O_WRONLY)
+        try:
+            build = build[os.write(fd, build):]
+        finally:
+            os.close(fd)
     except OSError:
         fd = os.open("%s.xml" % name, os.O_CREAT | os.O_WRONLY)
-    try:
-        while build:
+        try:
             build = build[os.write(fd, build):]
-    finally:
-        os.close(fd)
+        finally:
+            os.close(fd)
 
 
 if __name__ == "__main__":
